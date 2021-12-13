f32fc860995af09794280576913a448ef14a4b98
Afjal Sharif <67854373+afjal-sharif@users.noreply.github.com>
2021-12-14 04:06:48 +0600

    Update __init__.py

--- VideoEncoder/utils/__init__.py
+++ VideoEncoder/utils/__init__.py
@@ -20,24 +20,30 @@ from pyrogram.errors import RPCError
 from .. import app as a
 from . import ffmpeg, progress, tasks, utils
 
-sauce = '''<b>VideoEncoder - a telegram bot for compressing/encoding videos in h264 format.</b>
-Copyright (c) 2021 WeebTime/VideoEncoder
+sauce = '''Encode Settings Guide 
+RESOLUTION: 
+>> Source 
+>> 1080 
+>> 720 
+>> 480 
+>> 360 
+PRESET:
+>> uf = ultrafast
+>> sf = superfast
+>> vf = veryfast
+>> f = fast
+>> m = medium
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published
-by the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program. If not, see .'''
+AUDIO (Codec):
+>> opus
+>> aac
+>> copy
+CRF: It's percentage of compress rate
+>>  15 - 40 
+'''
 
 
-@a.on_message(filters.command('so' 'ur' 'ce'))
+@a.on_message(filters.command('guide'))
 async def g_s(_, message):
     try:
         await message.reply(text=sauce, reply_markup=utils.output)
