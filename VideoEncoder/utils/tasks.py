71fcf4c7efc8b2710e6731cb0f0dad80aa1237cc
Afjal Sharif <67854373+afjal-sharif@users.noreply.github.com>
2021-12-14 18:06:39 +0600

    Update tasks.py

--- VideoEncoder/utils/tasks.py
+++ VideoEncoder/utils/tasks.py
@@ -30,7 +30,7 @@ from .. import (audio, crf, doc_thumb, preset, resolution, sudo_users, tune,
                 upload_doc)
 
 server = "<b>CPU:</b> {psutil.cpu_percent()}% || <b>RAM:</b> {psutil.virtual_memory().percent}%"
-donewithcurrentprofile = 'Video Encoded Successfully! \n\n <b>Encode Settings</b> Tune: <code>{tune}</code> | <code>Preset: {preset}</code> Audio: <code>{audio} | <code>CRF: {crf}</code> Resolution: <code>{resolution}</code>'
+donewithcurrentprofile = 'Video Encoded Successfully! \n\n <b>Encode Settings</b>Resolution: <code>{resolution}</code> | <code>CRF: {crf}</code> \n Tune: <code>{tune}</code> | <code> Audio:{audio} </code> \n<code>Preset: {preset}</code>'
 async def on_task_complete():
     del data[0]
     if len(data) > 0:
